43afbe0ee186a01187571e7314ce2804cdb19a38
shivrajp130@gmail.com <Appleios55>
2022-08-15 17:24:24 +0530

    Improved ui tests and added test strategy. All tests are passing on this commit.

--- app/src/androidTest/java/com/shapegames/animals/views/home/MainActivityTest.kt
+++ app/src/androidTest/java/com/shapegames/animals/views/home/MainActivityTest.kt
@@ -43,8 +43,19 @@ class MainActivityTest {
     var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
 
+    /**
+     * [HomeFragment] Strategy
+     * - First we will check static text/views visibility
+     * - Validate if breed list is not empty.
+     * - Check if the icons (Like) are displayed
+     * - Check if click on like is taking to [LikedDogsFragment]
+     * - From LikedDogsFragment press back
+     * - Validate if it is taking back to [HomeFragment] and list is not empty
+     */
+
     @Test
-    fun test_like_dog_working() {
+    fun test_home_fragment() {
+
         onView(withText("Dog Breeds"))
             .check(matches(isDisplayed()))
 
@@ -54,55 +65,30 @@ class MainActivityTest {
         onView(withId(R.id.likedDogsIv))
             .check(matches(isDisplayed()))
 
-        //Working
-        onView(withId(R.id.breedsRv))
-            // scrollTo will fail the test if no item matches.
-            .perform(RecyclerViewActions.actionOnItemAtPosition<BreedsViewHolder>(2, click()))
+        sleep(500)
 
-        onView(withText("Dog Breeds"))
-            .check(matches(isDisplayed()))
+        assertListNotEmpty(R.id.breedsRv)
 
-        onView(withId(R.id.likedDogsIv))
-            .check(matches(isDisplayed()))
-
-        clickListItem(R.id.dogsRv, 4)
-        sleep(1000)
-
-        clickListItem(R.id.dogsRv, 8)
-        sleep(1000)
-
-        clickListItem(R.id.dogsRv, 1)
-        sleep(1000)
-
-        clickListItem(R.id.dogsRv, 7)
-
-        sleep(2000)
-
+        sleep(500)
 
         onView(withId(R.id.likedDogsIv))
             .check(matches(isDisplayed())).perform(click())
 
-        assertListNotEmpty(R.id.likedDogsRv)
-        //assertListItemCount(R.id.likedDogsRv, 4)
+        sleep(500)
 
-        /* //Working
-         onView(withId(R.id.dogsRv))
-             // scrollTo will fail the test if no item matches.
-             .perform(RecyclerViewActions.actionOnItemAtPosition<BreedsViewHolder>(1, click()))
+        onView(withText("Liked Dogs"))
+            .check(matches(isDisplayed()))
 
-         //Working
-         onView(withId(R.id.dogsRv))
-             // scrollTo will fail the test if no item matches.
-             .perform(RecyclerViewActions.actionOnItemAtPosition<BreedsViewHolder>(2, click()))
+        sleep(500)
 
-         onView(withId(R.id.likedDogsIv))
-             .check(matches(isDisplayed())).perform(click())*/
+        onView(withId(R.id.backIv))
+            .check(matches(isDisplayed())).perform(click())
 
+        sleep(1000)
 
-    }
+        assertListNotEmpty(R.id.breedsRv)
 
-    @Test
-    fun test_dogs_list_loaded() {
+        sleep(500)
 
         onView(withText("Dog Breeds"))
             .check(matches(isDisplayed()))
@@ -110,39 +96,69 @@ class MainActivityTest {
         onView(withText("Choose your favourite breed"))
             .check(matches(isDisplayed()))
 
+        sleep(500)
+
+        //Working
+        onView(withId(R.id.breedsRv))
+            // scrollTo will fail the test if no item matches.
+            .perform(RecyclerViewActions.actionOnItemAtPosition<BreedsViewHolder>(1, click()))
+
+        onView(withText("Dog Breeds"))
+            .check(matches(isDisplayed()))
+
         onView(withId(R.id.likedDogsIv))
             .check(matches(isDisplayed()))
 
-        sleep(2000)
+        sleep(500)
 
-        assertListNotEmpty(R.id.likedDogsRv)
+        onView(withId(R.id.backIv))
+            .check(matches(isDisplayed())).perform(click())
 
-        sleep(2000)
+        sleep(500)
 
-        clickListItem(R.id.breedsRv, 4)
-
-        /*//Working
-        onView(withId(R.id.breedsRv))
-            // scrollTo will fail the test if no item matches.
-            .perform(RecyclerViewActions.actionOnItemAtPosition<BreedsViewHolder>(2, click()))
-        */
-    }
-
-    @Test
-    fun test_breeds_list_loaded() {
         onView(withText("Dog Breeds"))
             .check(matches(isDisplayed()))
 
         onView(withText("Choose your favourite breed"))
             .check(matches(isDisplayed()))
 
-        onView(withId(R.id.likedDogsIv))
-            .check(matches(isDisplayed()))
+        sleep(500)
+
+        assertListNotEmpty(R.id.breedsRv)
+
+        sleep(500)
+
+    }
+
+
+    /**
+     * [DogsDetailsFragment] Strategy
+     * - First we will Validate breed list is not empty.
+     * - Click on first breed.
+     * - Validate it is navigating to [DogsDetailsFragment]
+     * - Validate static title/icons are visible
+     * - Click on Liked icon.
+     * - Validate if it is navigating to [LikedDogsFragment]
+     * - Click on back button on [LikedDogsFragment]
+     * - Validate if it is navigating to [DogsDetailsFragment]
+     * - Validate list is not empty.
+     * - Like image at index 1 & Validate if red filled heart icon updated.
+     * - unLike image at index 1 & Validate if unfilled heart icon updated.
+     */
+
+    @Test
+    fun test_dog_details_fragment() {
+
+        sleep(100)
+
+        assertListNotEmpty(R.id.breedsRv)
+
+        sleep(200)
 
         //Working
         onView(withId(R.id.breedsRv))
             // scrollTo will fail the test if no item matches.
-            .perform(RecyclerViewActions.actionOnItemAtPosition<BreedsViewHolder>(2, click()))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<BreedsViewHolder>(1, click()))
 
         onView(withText("Dog Breeds"))
             .check(matches(isDisplayed()))
@@ -150,13 +166,174 @@ class MainActivityTest {
         onView(withId(R.id.likedDogsIv))
             .check(matches(isDisplayed()))
 
-        //Working
-        onView(withId(R.id.dogsRv))
-            // scrollTo will fail the test if no item matches.
-            .perform(RecyclerViewActions.actionOnItemAtPosition<BreedsViewHolder>(2, click()))
+        sleep(1000)
+
+        assertListNotEmpty(R.id.dogsRv)
+
+        sleep(200)
+
+        onView(withId(R.id.likedDogsIv))
+            .check(matches(isDisplayed())).perform(click())
+
+        sleep(1000)
+
+        onView(withId(R.id.backIv))
+            .check(matches(isDisplayed())).perform(click())
+
+        sleep(1000)
+
+        assertListNotEmpty(R.id.dogsRv)
+
+        sleep(200)
+
+        onView(withText("Dog Breeds"))
+            .check(matches(isDisplayed()))
+
+
+        //Check if like/unlike is working
+        //----------------------------------------------------
+        sleep(500)
+
+        assertDrawableDisplayedAtPosition(
+            R.id.dogsRv,
+            1, R.id.likeIv, R.drawable.heart_empty
+        )
+
+        sleep(200)
+
+        clickListItem(R.id.dogsRv, 1)
+
+        sleep(200)
+
+        assertDrawableDisplayedAtPosition(
+            R.id.dogsRv,
+            1, R.id.likeIv, R.drawable.heart_filled
+        )
+
+        sleep(500)
+
+        clickListItem(R.id.dogsRv, 1)
+
+        sleep(500)
+
+        assertDrawableDisplayedAtPosition(
+            R.id.dogsRv,
+            1, R.id.likeIv, R.drawable.heart_empty
+        )
+
+        sleep(500)
+
+        onView(withId(R.id.backIv))
+            .check(matches(isDisplayed())).perform(click())
+
+        sleep(2000)
+
+        assertListNotEmpty(R.id.breedsRv)
+
+        sleep(100)
+
+        //----------------------------------------------------
 
     }
 
 
+    /**
+     * [LikedDogsFragment] Strategy
+     * - First we will Validate breed list is not empty.
+     * - Click on first breed.
+     * - Validate it is navigating to [DogsDetailsFragment]
+     * - Validate static title/icons are visible
+     * - Like image at index 1 & Validate if red filled heart icon updated.
+     *
+     * - Click on like icon.
+     * - Validate if it is navigating to [LikedDogsFragment]
+     * - Validate if list is showing liked image
+     *
+     * - Click on back button on [LikedDogsFragment]
+     * - Validate if it is navigating to [DogsDetailsFragment]
+     * - unLike image at index 1 & Validate if unfilled heart icon updated.
+     *
+     * - Click on like icon.
+     * - Validate if unliked image is removed.
+     */
+
+    @Test
+    fun test_liked_dogs_fragment() {
+        sleep(2000)
+
+        assertListNotEmpty(R.id.breedsRv)
+
+        sleep(500)
+
+        clickListItem(R.id.breedsRv, 1)
+
+        sleep(500)
+
+        onView(withText("Dog Breeds"))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.likedDogsIv))
+            .check(matches(isDisplayed()))
+        sleep(500)
+
+        clickListItem(R.id.dogsRv, 1)
+
+        sleep(200)
+
+        assertDrawableDisplayedAtPosition(
+            R.id.dogsRv,
+            1, R.id.likeIv, R.drawable.heart_filled
+        )
+
+        sleep(200)
+
+        onView(withId(R.id.likedDogsIv))
+            .check(matches(isDisplayed())).perform(click())
+
+        sleep(500)
+
+        onView(withText("Liked Dogs"))
+            .check(matches(isDisplayed()))
+
+        sleep(200)
+
+        assertListNotEmpty(R.id.likedDogsRv)
+
+        sleep(500)
+
+        onView(withId(R.id.backIv))
+            .check(matches(isDisplayed()))
+
+
+        onView(withId(R.id.backIv))
+            .check(matches(isDisplayed())).perform(click())
+
+        sleep(500)
+
+        onView(withText("Dog Breeds"))
+            .check(matches(isDisplayed()))
+
+        sleep(500)
+
+        clickListItem(R.id.dogsRv, 1)
+
+        sleep(200)
+
+        assertDrawableDisplayedAtPosition(
+            R.id.dogsRv,
+            1, R.id.likeIv, R.drawable.heart_empty
+        )
+
+        sleep(500)
+
+        onView(withId(R.id.likedDogsIv))
+            .check(matches(isDisplayed())).perform(click())
+
+        sleep(500)
+
+        assertListItemCount(R.id.likedDogsRv, 0)
+
+        sleep(500)
+    }
 
 }
